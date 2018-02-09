4e9bc852044859c948f449f03d7677572e17003e
mpaauw <matt.paauw@gmail.com>
2018-02-08 20:21:21 -0800

    Added boilerplate Collections class.

--- src/main/java/engine/collection/Collections.java
+++ src/main/java/engine/collection/Collections.java
@@ -1,10 +1,20 @@
 package engine.collection;
 
+import util.Constants;
+
+import java.util.Collection;
+import java.util.Random;
+
 /**
  * Contains methods for generating collections.
  */
-public class Collections {
+public class Collections<T> {
 
+    private long upperBoundSize = Constants.UPPER_BOUND_SIZE;
+    private Random rand = new Random();
 
+    public Collections(long upperBoundSize) {
+        this.upperBoundSize = upperBoundSize;
+    }
 
 }
